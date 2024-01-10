3c03842235df8b4b0048c88913bf20e0e4c0a8ac
AkayRS <57268397+AkayRS@users.noreply.github.com>
2024-01-10 13:01:39 +0100

    ciao

--- processo.php
+++ processo.php
@@ -1,12 +1,3 @@
 <?php
-    // Verifica se i dati del modulo sono stati inviati
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Recupera i dati dal modulo
-        $name = $_POST["name"];
-        $email = $_POST["email"];
-
-        // Puoi ora utilizzare $name e $email come preferisci
-        echo "Name: " . $name . "<br>";
-        echo "Email: " . $email;
-    }
+    echo "Ciao";
 ?>
